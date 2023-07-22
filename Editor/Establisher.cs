9e101ce4b0f336c414e4369358ee18bc77c9de3e
Hibnu Hishath <hibnuhishath1@gmail.com>
2023-07-23 01:16:30 +0530

    Fixing an issue where the ENABLE_DEFINE_MANAGER define would exist even after the package is removed by the user

--- Editor/Establisher.cs
+++ Editor/Establisher.cs
@@ -1,6 +1,7 @@
 #if UNITY_EDITOR
 
 using UnityEditor;
+using UnityEditor.PackageManager;
 
 namespace Hibzz.DefineManager
 {
@@ -14,6 +15,32 @@ namespace Hibzz.DefineManager
 		{
 			// Check and add the script
 			Manager.AddDefine("ENABLE_DEFINE_MANAGER");
+
+			// Subscribe to the package manager event to monitor if the define
+			// manager is requested to be removed, so that appropriate cleanup
+			// can be performed
+			Events.registeringPackages += HandlePackageRemoval;
+		}
+
+		/// <summary>
+		/// Handles the removal of Define Manager and performs cleanup
+		/// </summary>
+		/// <param name="registrationInfo">The arguments passed by the package manager event</param>
+		static void HandlePackageRemoval(PackageRegistrationEventArgs registrationInfo)
+		{
+			// look through packages that are queued to be removed
+			foreach(var package in registrationInfo.removed)
+			{
+				// when the define manager is being removed by the user,
+				// perform a cleanup and remove the ENABLE_DEFINE_MANAGER
+				// define that indicates other packages and scripts that the
+				// define manager is installed
+				if(package.name == "com.hibzz.definemanager")
+				{
+					Manager.RemoveDefine("ENABLE_DEFINE_MANAGER");
+					return;
+				}
+			}	
 		}
 	}
 }
